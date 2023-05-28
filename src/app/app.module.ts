bb7139087a27b2d7ae9b1b441d5284ff4c530753
Sydney S <sydney.silverman3@gmail.com>
2023-05-28 09:37:05 -0400

    added material imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatButtonModule } from '@angular/material/button';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ForumComponent } from './forum/forum.component';
@@ -20,7 +22,9 @@ import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
